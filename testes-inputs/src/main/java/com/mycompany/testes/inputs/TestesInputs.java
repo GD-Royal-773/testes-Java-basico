afe5bf233bdddce3d263e5e8bacdcbd7b9c7d8c2
GD-Royal-773 <105356928+GD-Royal-773@users.noreply.github.com>
2025-04-16 17:34:20 -0300

    Primeiro teste de commit

---
+++ testes-inputs/src/main/java/com/mycompany/testes/inputs/TestesInputs.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testes.inputs;
+
+/**
+ *
+ * @author Royal
+ */
+public class TestesInputs {
+
+    public static void main(String[] args) {
+        System.out.println("Testes de java basico");
+    }
+}
